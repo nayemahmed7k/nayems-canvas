7d2caa48db3aeb5fcc4b8636ee755d90d90d62c6
Nayem Ahmed <144725995+nayemahmed7k@users.noreply.github.com>
2023-10-17 01:04:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function notRes(){
     alert("This page may not responsible for small screen/device");
 }
+function newWeb(){
+    alert("We are moving to another website");
+}
+function newWebNotRes(){
+    alert("Moving to a another website and it may not be responsive for smaller screen");
+}
