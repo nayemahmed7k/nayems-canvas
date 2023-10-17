e7330db5a2e4c7962db9581f6bfc506f2619b234
Nayem Ahmed <144725995+nayemahmed7k@users.noreply.github.com>
2023-10-18 02:09:58 +0600

    Update pagesScript.js

--- resources/pagesScript.js
+++ resources/pagesScript.js
@@ -38,3 +38,53 @@ function shadow(sb){
     }
 }
 
+
+/* HTML Javascript Page */
+function fun1() {
+    document.getElementById('t1').innerHTML= "Did you notice that?";
+}
+function fun3() {
+    document.getElementById('t3').style.backgroundColor= "green";
+}
+function color(codE) {
+    if(codE==0) {
+        document.getElementById('t2').style.color= "red";
+    }
+    else if(codE==1) {
+        document.getElementById('t2').style.color= "green";
+    }
+    else if(codE==2) {
+        document.getElementById('t2').style.color= "blue";
+    }
+}
+
+function fun4(sw) {
+    if(sw==1) {
+        document.getElementById('light').src= "/resources/pictures/lighton.png";
+    }
+    else if(sw==0) {
+        document.getElementById('light').src= "/resources/pictures/lightoff.png";
+    }
+}
+function hBarF(sz) {
+    if(sz==0) {
+        document.getElementById('hBar').style.width= "50%";
+    }
+    else if(sz==1) {
+        document.getElementById('hBar').style.width= "100%";
+    }
+    else if(sz==3) {
+        document.getElementById('hBar').style.display= "none";
+        document.getElementById('hBarButtons').style.display= "none";
+        document.getElementById('hBarPview').style.display= "block";
+    }
+    else if(sz==4) {
+        document.getElementById('hhBar').innerHTML= "Horizontal Bar";
+        document.getElementById('hhBar').style.width= "100%";
+        document.getElementById('hhBar').style.backgroundColor= "whitesmoke";
+        document.getElementById('hhBar').style.borderStyle= "ridge";
+        document.getElementById('hhBar').style.color= "gray";
+        document.getElementById('hhBar').style.padding= "10px";
+        document.getElementById('hhBar').style.textAlign= "center";
+    }
+}
