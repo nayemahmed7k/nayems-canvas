c972eccbaefd2644b039f9d2f1f37c8a0c41a1fb
Nayem Ahmed <144725995+nayemahmed7k@users.noreply.github.com>
2023-12-04 02:26:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,48 @@ function underM(){
         </script>
     */
 }
+function leaveFun(){
+    alert("You're about to leave my personal website");
+}
+
+
+//about page
+var abA= 0;
+function panelFun2(){
+    var abB=document.getElementById('hPanel2');
+    var abC=document.getElementById('hPanel2B');
+    if(abA==0){
+        abB.style.display= "block";
+        abC.innerHTML= "Hide";
+        abC.style.backgroundColor= "blue";
+        abC.style.color= "white";
+        abA++;
+    }
+    else{
+        abB.style.display= "none";
+        abC.innerHTML= "Contents";
+        abC.style.backgroundColor= "#eee";
+        abC.style.color= "black";
+        abA--;
+    }
+}
+
+
+//homepage
+var a= 0;
+function panelFun(){
+    var b=document.getElementById('h_panel');
+    var c=document.getElementById('angle');
+    if(a==0){
+        b.style.display= "block";
+        c.src= "/media/angle3.png";
+        a++;
+    }
+    else{
+        b.style.display= "none";
+        c.src= "/media/angle2.png";
+        a--;
+    }
+}
+
+
