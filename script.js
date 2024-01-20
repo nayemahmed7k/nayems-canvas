9f8c930046ec543d6b14433e81a4333ba8d10619
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-01-21 00:09:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function leaveFun(){
     alert("You're about to leave my personal website");
 }
+var a = document.getElementById('h_panel1');
+var count = 0;
+function showStudyF(){
+    if(count==0){
+        a.style.display = "block";
+        count++;
+    } else{
+        a.style.display = "none";
+        count--;
+    }
+}
