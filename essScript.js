05b60dc8881b93789d578dc238742a2dfdafea4f
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-01-30 00:03:30 +0600

    Update essScript.js

--- essScript.js
+++ essScript.js
@@ -1,2 +1,8 @@
-/* ---------------Home Page--------------- */
+// Homepage Ess
+var fWebName = document.getElementById('f-webName');
+var mD = document.getElementById('monthDate');
+var pfV = document.getElementById('pfVersion');
 
+fWebName.innerHTML = "@Nayem's Canvas";
+mD.innerHTML = "January 2024";
+pfV.innerHTML = "PF v 3.0";
