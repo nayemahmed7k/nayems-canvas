b3509d00ae2b1145be01e3c2b4347a1972dd4ca0
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-08-02 22:27:37 +0600

    Latest Release : Nayem's Canvas - A Personal Blog Website

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nayems-canvas
 This is a portfolio website of Nayem Ahmed
 Link https://nayemscanvas.xyz
+
+## Note : Entire web layout chnage on 2 August 2024, decided to create a personal blog (before nayems canvas was a portfolio website)
--- essScript.js
+++
@@ -1,8 +0,0 @@
-// Homepage Ess
-var fWebName = document.getElementById('f-webName');
-var mD = document.getElementById('monthDate');
-var pfV = document.getElementById('pfVersion');
-
-fWebName.innerHTML = "@Nayem's Canvas";
-mD.innerHTML = "January 2024";
-pfV.innerHTML = "PF v 3.0";
